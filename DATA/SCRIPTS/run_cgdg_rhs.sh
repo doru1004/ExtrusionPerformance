02477ee2bfc6f817a259ee3e287007fda1cbfa04
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2016-09-06 16:45:26 +0100

    Remove gnuplot plots.

--- DATA/SCRIPTS/run_cgdg_rhs.sh
+++ DATA/SCRIPTS/run_cgdg_rhs.sh
@@ -86,12 +86,6 @@ greedy_reader="${base}/greedy_compact_reader.py"
 
 python $greedy_reader $res $gnuplot_dir $side $vectorized $likwid_dir
 
-for entry in "$gnuplot_dir"/*."gnuplot"
-do
-    gnuplot $entry
-done
-
-# TODO: Change this to be generic for RHS and LHS
 cat "$gnuplot_dir"/RHS__*_1.txt > "${rfl_dir}/RHS_1.txt"
 cat "$gnuplot_dir"/RHS__*_6.txt > "${rfl_dir}/RHS_6.txt"
 cat "$gnuplot_dir"/RHS__*_12.txt > "${rfl_dir}/RHS_12.txt"
